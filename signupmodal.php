a7abee72780b4f88d174434f338fedd9230a3ccd
Anne Bliton <anne@inthesun.net>
2015-01-13 14:35:04 -0800

    Added modal tabs

---
+++ signupmodal.php
@@ -0,0 +1,30 @@
+<div id="signup-panel">
+    <form id="signup-form">
+        <div class="form-group">
+            <!-- <label for="recipient-name" class="control-label">Recipient:</label> -->
+            <div id='email-field'>
+                <span class="glyphicon glyphicon-envelope"></span>
+                <input type="text" class="form-control shorten fa-envelope" id="recipient-name" placeholder="EMAIL" />
+            </div>
+            <div id='username-field'>
+                <span class='glyphicon glyphicon-user'></span>
+                <input type="text" class="form-control shorten " placeholder="CREATE USERNAME" />
+            </div>
+            <div id='createPassword-field'>
+                <span class="glyphicon glyphicon-lock"></span>
+                <input type="text" class="form-control shorten" placeholder="CREATE PASSWORD" />
+            </div>
+            <div id='verifyPassword-field'>
+                <span class='glyphicon glyphicon-lock'></span>
+                <input type="text" class="form-control shorten " placeholder="VERIFY PASSWORD" />
+            </div>
+
+            <div id='checkbox-field'>
+                <label><input type='checkbox' id='ageVerify-box' /> I am 13+</label>
+            </div>
+        </div>
+        <div id="register-button-field">
+            <button type="button" class="btn btn-success" id="doodad-submit">BECOME A DOODADIfier</button>
+        </div>
+    </form>
+</div>
