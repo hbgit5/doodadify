b429c3ba28bb7e0aa1ae7d3aa762a343e34e8929
Thi Nguyen <thiman@gmail.com>
2015-01-14 16:30:18 -0800

    More cleanup. Integrated auth modal and forms

--- index.php
+++ index.php
@@ -5,44 +5,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Doodadify - Your Life As Doodads</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
     <link href='http://fonts.googleapis.com/css?family=Handlee' rel='stylesheet' type='text/css'>
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <script src="http://code.jquery.com/jquery-2.1.3.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
 </head>
 
 <body>
     <header>
-        <!--
-        <div class="social-nav list-clean">
-            <ul id="social">
-                <li>
-                    <img src="img/fb.png">
-                </li>
-                <li>
-                    <img src="img/instagram.png">
-                </li>
-                <li>
-                    <img src="img/gplus.png">
-                </li>
-                <li>
-                    <img src="img/twitter.png">
-                </li>
-                <li>
-                    <img src="img/pinterest.png">
-                </li>
-            </ul>
-        </div>
-        -->
         <img id="main-logo" src="img/doodadify_logo.png"></img>
-        
+
     </header>
     <main>
-        
+
         <section id="canvas-container">
             <div class="sign-in-btns">
-            <a href="#">LogIn</a>
-            <a href="#">SignUp</a>
-        </div>
+                <a href="#" data-toggle="modal" data-target="#auth_modal" >Login</a>
+                <a href="#" data-toggle="modal" data-target="#auth_modal">Sign Up</a>
+            </div>
             <div class="main-canvas">
                 <div class="top-bar">
                     <button id="canvas-del">Delete</button>
@@ -50,7 +32,6 @@
                     <button id="canvas-upload">Upload</button>
                     <button id="canvas-save">SAVE</button>
                     <button id="canvas-share">SHARE</button>
-                    
                 </div>
 
                 <div id="canvas-back-image">
@@ -58,7 +39,8 @@
                 </div>
 
                 <div id="canvas-doodad-picker">
-                    <p class="" id="picker-text">DOODAD's to d<span>ify your life - </span><span>Just add or search </span></p>
+                    <p class="" id="picker-text">DOODAD's to d<span>ify your life - </span><span>Just add or search </span>
+                    </p>
 
                     <div class="doodad-boxes">
                         <div class="neg-degree-10 doodad-cat-box" id="left-arrow"></div>
@@ -84,10 +66,40 @@
             <button class="doodad-splatter">Unknown Splatter!</button>
         </section>
 
-<footer>
-    <p>Copyright © 2015_ Doodadify - All Rights Reserved</p>
-</footer>
+        <footer>
+            <p>Copyright © 2015_ Doodadify - All Rights Reserved</p>
+        </footer>
     </main>
+
+    <div class="modal fade" id="auth_modal" tabindex="-1" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <img src="img/doodadify_logo.png" width="125" height="30" />
+                    <button type="button" class="close close-solid" data-dismiss="modal" aria-label="Close"><span id="remove-button"><img src="img/close.png" width="40" /></span> 
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <ul id="tabs" class="nav nav-tabs" role="tablist">
+                        <li class="active" role="presentation">
+                            <a href="#login-tab" data-toggle="tab" role="tab" aria-controls="login-tab" class="modal-tab">LOGIN</a>
+                        </li>
+                        <li role="presentation">
+                            <a href="#signup-tab" data-toggle="tab" aria-controls="signup-tab" class="modal-tab">SIGN UP</a>
+                        </li>
+                    </ul>
+                    <div id="modal-container" class="tab-content">
+                        <div id="login-tab" class="tab-pane active" role="tabpanel">
+                            <?php include 'loginmodal.php'; ?>
+                        </div>
+                        <div id="signup-tab" class="tab-pane" role="tabpanel">
+                            <?php include 'signupmodal.php'; ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
     <script src="javascript.js "></script>
